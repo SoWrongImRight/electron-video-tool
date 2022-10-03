2c2728ab5e64af25984c292406fdd9736b1557cf
Russ Carroll <info@rcarroll.dev>
2022-10-03 12:01:40 -0400

    Finished project.

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+let mainWindow;
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+app.disableHardwareAcceleration()
 
 app.on('ready', () => {
   
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({ webPreferences: {
+    nodeIntegration: true,
+    contextIsolation: false,
+  }});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
 });
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+
+  })
+})
